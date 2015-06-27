6a18a1d54e5e7f51b77ef6eb36264c37644eb47b
Steve Briskin <steve.briskin@10gen.com>
2015-06-27 09:38:09 -0400

    split charts

--- reefaiui/chart.py
+++ reefaiui/chart.py
@@ -25,7 +25,7 @@ html = Template('''\
   <head>
     <title>Reef Ai Temp and Ph readings</title>
     <style>
-      #chart {
+      .chart {
         min-height: 300px;
       }
     </style>
@@ -42,7 +42,23 @@ html = Template('''\
       <br>
       <label>Last Reading</label> <label id="latestts" class="label label-primary"></label>
     </div>
-    <div id="chart"></div>
+
+    <p>
+
+    <div>
+      <label class="label label-default">Combined</label>
+      <div id="combinedchart" class="chart"></div>
+      
+      <br>
+
+      <label class="label label-default">Temp</label>
+      <div id="tempchart" class="chart"></div>
+
+      <br>
+
+      <label class="label label-default">Temp</label>
+      <div id="phchart" class="chart"></div>
+    </div>
   </body>
   <script
     src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js">
@@ -55,7 +71,9 @@ html = Template('''\
   </script>
 
   <script>
-  var chart;
+  var tempchart;
+  var phchart;
+  var combinedchart;
 
   function get_data() {
     $.ajax({
@@ -67,14 +85,27 @@ html = Template('''\
   }
 
   function on_data(data) {
-    chart.setData([
-      {label: 'pH', data: data.phvalues, yaxis:1}, 
-      {label: 'temp', data: data.tempvalues, yaxis:2}
+    combinedchart.setData([
+      {label: 'pH', data: data.phvalues, yaxis:1, color: 'red'}, 
+      {label: 'temp', data: data.tempvalues, yaxis:2, color: 'blue'}
     ]);
-    
 
-    chart.setupGrid();
-    chart.draw();
+    combinedchart.setupGrid();
+    combinedchart.draw();
+
+    phchart.setData([
+      {label: 'pH', data: data.phvalues, color: 'red'}, 
+    ]);
+
+    phchart.setupGrid();
+    phchart.draw();
+
+    tempchart.setData([
+      {label: 'temp', data: data.tempvalues, color: 'blue'}, 
+    ]);
+
+    tempchart.setupGrid();
+    tempchart.draw();
 
     $('#latestph').text(data.latestph);
     $('#latesttemp').text(data.latesttemp);
@@ -84,7 +115,7 @@ html = Template('''\
   }
 
   $(function() {
-    chart = $.plot("#chart", [], 
+    combinedchart = $.plot("#combinedchart", [], 
       {xaxis: {mode: "time"},
        yaxes: [{ labelWidth: 40,
                   autoscaleMargin: 0.07,
@@ -92,12 +123,34 @@ html = Template('''\
                 },
                 { labelWidth: 40,
                   autoscaleMargin: 0.07,
-                  position: 'left'
+                  position: 'right'
                 }
               ],
       grid: {hoverable: true, autoHighlight: true },
       selection:{mode:"x"}
       });
+
+    phchart = $.plot("#phchart", [], 
+      {xaxis: {mode: "time"},
+       yaxes: [{ labelWidth: 40,
+                  autoscaleMargin: 0.07,
+                  position: 'right'
+                },
+              ],
+      grid: {hoverable: true, autoHighlight: true },
+      selection:{mode:"x"}
+    });
+
+    tempchart = $.plot("#tempchart", [], 
+      {xaxis: {mode: "time"},
+       yaxes: [{ labelWidth: 40,
+                  autoscaleMargin: 0.07,
+                  position: 'right'
+                },
+              ],
+      grid: {hoverable: true, autoHighlight: true },
+      selection:{mode:"x"}
+    });
 
     get_data();
   });
@@ -136,7 +189,7 @@ def data():
     parsedPh = [(int(datetime.strptime(row['_id'],'%Y-%m-%dT%H:%M:%S').strftime('%s')) * 1000 , row['ph']) for row in entries]
     parsedTemp = [(int(datetime.strptime(row['_id'],'%Y-%m-%dT%H:%M:%S').strftime('%s')) * 1000 , row['temp']) for row in entries]
 
-    latestReading = mongo['reef_ai']['readings'].find({}).sort('_id', DESCENDING).limit(1)[0]
+    latestReading = mongo['reef_ai']['readings'].find({}).sort('ts', DESCENDING).limit(1)[0]
 
     #print latestReading
 
